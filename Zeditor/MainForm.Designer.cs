3e3a6a11082578585ce6da3efcb4d9ddbbf41572
starfrost <mario64crashed@gmail.com>
2023-01-15 01:41:48 +0000

    fix csproj

--- Zeditor/MainForm.Designer.cs
+++ Zeditor/MainForm.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "Zombono Map Editor (Zeditor)";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
